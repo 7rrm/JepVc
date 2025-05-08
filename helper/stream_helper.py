b96e960e67f06aee7bd9a0a04b464c83ef44c04b
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-05-08 20:11:06 +0300

    Update stream_helper.py

--- helper/stream_helper.py
+++ helper/stream_helper.py
@@ -1,21 +1,28 @@
+import os
+import glob
+import random
 import re
 from enum import Enum
-
 from requests.exceptions import MissingSchema
 from requests.models import PreparedRequest
 from JoKeRUB.utils import runcmd
 from yt_dlp import YoutubeDL
 
-
 class Stream(Enum):
     audio = 1
     video = 2
 
-
 yt_regex_str = "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
-
 yt_regex = re.compile(yt_regex_str)
 
+def get_cookies_file():
+    """الحصول على ملف كوكيز عشوائي من مجلد karar"""
+    folder_path = f"{os.getcwd()}/karar"
+    txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
+    if not txt_files:
+        raise FileNotFoundError("No .txt files found in the specified folder.")
+    cookie_txt_file = random.choice(txt_files)
+    return cookie_txt_file
 
 def check_url(url):
     prepared_request = PreparedRequest()
@@ -25,17 +32,20 @@ def check_url(url):
     except MissingSchema:
         return False
 
-
 async def get_yt_stream_link(url, audio_only=False):
+    """الحصول على رابط البث مع دعم ملفات الكوكيز"""
+    cookies_file = get_cookies_file()
     if audio_only:
         return (
-            await runcmd(f"yt-dlp --no-warnings --geo-bypass -f bestaudio -g {url}")
+            await runcmd(f"yt-dlp --no-warnings --geo-bypass --cookies {cookies_file} -f bestaudio -g {url}")
         )[0]
-    return (await runcmd(f"yt-dlp --no-warnings --geo-bypass -f best -g {url}"))[0]
-
+    return (await runcmd(f"yt-dlp --no-warnings --geo-bypass --cookies {cookies_file} -f best -g {url}"))[0]
 
 async def video_dl(url, title):
+    """تحميل الفيديو مع استخدام ملفات الكوكيز"""
     path = f"temp/{title.replace(' ', '_')}.mp4"
+    cookies_file = get_cookies_file()
+    
     video_opts = {
         "format": "best",
         "addmetadata": True,
@@ -44,6 +54,7 @@ async def video_dl(url, title):
         "prefer_ffmpeg": True,
         "geo_bypass": True,
         "nocheckcertificate": True,
+        "cookiefile": cookies_file,  # إضافة ملف الكوكيز
         "postprocessors": [
             {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"},
             {"key": "FFmpegMetadata"},
