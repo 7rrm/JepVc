9eaa9b2351efbc416c906acf2d73eba92af7629b
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-05-09 09:39:32 +0300

    Update vcp_helper.py

--- helper/vcp_helper.py
+++ helper/vcp_helper.py
@@ -65,7 +65,7 @@ class jepthonvc:
                 await self.client(
                     functions.phone.CreateGroupCallRequest(
                         peer=chat,
-                        title="آراس",
+                        title="الجوكر 🤡",
                     )
                 )
                 await self.join_vc(chat=chat, join_as=join_as)
