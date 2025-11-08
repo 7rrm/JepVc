0ea4e261492c81a6fc0805bdb69a62e89998d0ca
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-11-08 23:50:23 +0300

    Update vcplayer.py

--- vcplayer.py
+++ vcplayer.py
@@ -266,3 +266,46 @@ async def skip_stream(event):
     res = await vc_player.skip()
     await edit_delete(event, res, time=30)
     
+
+@l313l.ar_cmd(
+    pattern="فيد ?(-f)? ?([\S ]*)?",
+    command=("فيد", plugin_category),
+    info={
+        "header": "لتشغيل فيديو في المكالمة الصوتية",
+        "description": "لتشغيل فيديو في المكالمة الصوتية",
+        "flags": {
+            "-f": "التشغيل الإجباري وإيقاف التشغيل الحالي",
+        },
+        "usage": [
+            "{tr}فيد (بالرد على فيديو)",
+            "{tr}فيد (رابط يوتيوب)",
+            "{tr}فيد -f (رابط يوتيوب)",
+        ],
+        "examples": [
+            "{tr}فيد",
+            "{tr}فيد https://www.youtube.com/watch?v=example",
+            "{tr}فيد -f https://www.youtube.com/watch?v=example",
+        ],
+    },
+)
+async def play_video(event):
+    "لتشغيل فيديو في المكالمة الصوتية"
+    flag = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(2)
+    if input_str == "" and event.reply_to_msg_id:
+        input_str = await tg_dl(event)
+    if not input_str:
+        return await edit_delete(
+            event, "**قم بالرد على ملف فيديو او رابط يوتيوب**", time=20
+        )
+    if not vc_player.CHAT_ID:
+        return await edit_or_reply(event, "**`قم بالانضمام للمكالمة أولاً بأستخدام أمر `انضمام**")
+    if not input_str:
+        return await edit_or_reply(event, "لا يوجد مدخل لتشغيله في المكالمة")
+    await edit_or_reply(event, "**يتم الآن تشغيل الفيديو في الاتصال 📺**")
+    if flag:
+        resp = await vc_player.play_song(input_str, Stream.video, force=True)
+    else:
+        resp = await vc_player.play_song(input_str, Stream.video, force=False)
+    if resp:
+        await edit_delete(event, resp, time=30)
