03cafc5565217f2a11d681907597088d35fc797b
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-01-17 15:53:15 +0300

    Update vcp_helper.py

--- helper/vcp_helper.py
+++ helper/vcp_helper.py
@@ -43,7 +43,7 @@ class jepthonvc:
 
     async def join_vc(self, chat, join_as=None):
         if self.CHAT_ID:
-            return f"موجود بالفعل في المكالمة الصوتيةة {self.CHAT_NAME}"
+            return f"موجود بالفعل في المكالمة الصوتية {self.CHAT_NAME}"
         if join_as:
             try:
                 join_as_chat = await self.client.get_entity(int(join_as))
@@ -190,4 +190,4 @@ class jepthonvc:
             await self.app.resume_stream(self.CHAT_ID)
             self.PAUSED = False
         return f"- تم الاستئناف في {self.CHAT_NAME}"
-
+                
