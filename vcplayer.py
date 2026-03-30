d4c717d42567a6cdfbda98d04309b78548f3bd8f
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-03-30 11:43:06 +0300

    Update vcplayer.py

--- vcplayer.py
+++ vcplayer.py
@@ -8,13 +8,11 @@ from JoKeRUB.core.managers import edit_delete, edit_or_reply
 
 from .helper.stream_helper import Stream
 from .helper.tg_downloader import tg_dl
-from .helper.vcp_helper import jepthonvc
+from .helper.vcp_helper import ZedVC
 
-plugin_category = "extra"
+plugin_category = "المكالمات"
 
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
-OWNER_ID = l313l.uid
 
 vc_session = Config.VC_SESSION
 
@@ -26,7 +24,7 @@ else:
     vc_client = l313l
 
 vc_client.__class__.__module__ = "telethon.client.telegramclient"
-vc_player = jepthonvc(vc_client)
+vc_player = ZedVC(vc_client)
 
 asyncio.create_task(vc_player.start())
 
@@ -36,39 +34,12 @@ async def handler(_, update):
     await vc_player.handle_next(update)
 
 
-ALLOWED_USERS = set()
-
-
-@l313l.ar_cmd(
-    pattern="انضمام ?(\S+)? ?(?:-as)? ?(\S+)?",
-    command=("انضمام", plugin_category),
-    info={
-        "header": "To join a Voice Chat.",
-        "description": "To join or create and join a Voice Chat",
-        "note": "You can use -as flag to join anonymously",
-        "flags": {
-            "-as": "To join as another chat.",
-        },
-        "usage": [
-            "{tr}joinvc",
-            "{tr}joinvc (chat_id)",
-            "{tr}joinvc -as (peer_id)",
-            "{tr}joinvc (chat_id) -as (peer_id)",
-        ],
-        "examples": [
-            "{tr}joinvc",
-            "{tr}joinvc -1005895485",
-            "{tr}joinvc -as -1005895485",
-            "{tr}joinvc -1005895485 -as -1005895485",
-        ],
-    },
-)
+@l313l.ar_cmd(pattern="انضمام ?(\S+)? ?(?:-as)? ?(\S+)?")
 async def joinVoicechat(event):
-    "To join a Voice Chat."
     chat = event.pattern_match.group(1)
     joinas = event.pattern_match.group(2)
 
-    await edit_or_reply(event, "**جار الانضمام للمكالمة الصوتيةة**")
+    await edit_or_reply(event, "⚈ **جـارِ الانضمـام الى المكالمـة الصـوتيـه ...**")
 
     if chat and chat != "-as":
         if chat.strip("-").isnumeric():
@@ -78,235 +49,150 @@ async def joinVoicechat(event):
 
     if vc_player.app.active_calls:
         return await edit_delete(
-            event, f"لقد انضممت بالفعل الى {vc_player.CHAT_NAME}"
+            event, f"⚈ **انت منضـم مسبقـاً الـى** {vc_player.CHAT_NAME}"
         )
 
     try:
         vc_chat = await l313l.get_entity(chat)
     except Exception as e:
-        return await edit_delete(event, f'ERROR : \n{e or "UNKNOWN CHAT"}')
+        return await edit_delete(event, f'⚈ **خطـأ** : \n{e or "UNKNOWN CHAT"}')
 
     if isinstance(vc_chat, User):
-        return await edit_delete(
-            event, "لايمكنك استعمال اوامر الميوزك على الخاص فقط في المجموعات !"
-        )
+        return await edit_delete(event, "⚈ **لايمكنك استعمال اوامر الميوزك على الخاص فقط في المجموعات !**")
 
     if joinas and not vc_chat.username:
-        await edit_or_reply(
-            event, "**انت وين لكيت هل كلاوات حبيبي مو كتلك ميصير بلاتصال الخاص**"
-        )
+        await edit_or_reply(event, "⚈ **عـذراً عـزيـزي**\n⚈ **لم استطـع الانضمـام الى المكالمـة ✗**")
         joinas = False
 
     out = await vc_player.join_vc(vc_chat, joinas)
     await edit_delete(event, out)
 
 
-@l313l.ar_cmd(
-    pattern="غادر",
-    command=("غادر", plugin_category),
-    info={
-        "header": "To leave a Voice Chat.",
-        "description": "To leave a Voice Chat",
-        "usage": [
-            "{tr}leavevc",
-        ],
-        "examples": [
-            "{tr}leavevc",
-        ],
-    },
-)
+@l313l.ar_cmd(pattern="خروج")
 async def leaveVoicechat(event):
-    "To leave a Voice Chat."
     if vc_player.CHAT_ID:
-        await edit_or_reply(event, "** تدلل غادرت من الاتصال حبيبي ❤️ **")
+        await edit_or_reply(event, "⚈ **جـارِ مغـادرة المحـادثـة الصـوتيـه ...**")
         chat_name = vc_player.CHAT_NAME
         await vc_player.leave_vc()
-        await edit_delete(event, f"تمت المغادرة من {chat_name}")
+        await edit_delete(event, f"⚈ **تم مغـادرة المكـالمـه** {chat_name}")
     else:
-        await edit_delete(event, "** انا لست منضم الى الاتصال عزيزي ❤️**")
+        await edit_delete(event, "⚈ **لم تنضم بعـد للمكالمـه ؟!**")
 
 
-@l313l.ar_cmd(
-    pattern="قائمة_التشغيل",
-    command=("قائمة_التشغيل", plugin_category),
-    info={
-        "header": "To Get all playlist.",
-        "description": "To Get all playlist for Voice Chat.",
-        "usage": [
-            "{tr}playlist",
-        ],
-        "examples": [
-            "{tr}playlist",
-        ],
-    },
-)
+@l313l.ar_cmd(pattern="قائمة التشغيل")
 async def get_playlist(event):
-    "To Get all playlist for Voice Chat."
-    await edit_or_reply(event, "**جارِ جلب قائمة التشغيل ......**")
+    await edit_or_reply(event, "⚈ **جـارِ جلب قائمـة التشغيـل ...**")
     playl = vc_player.PLAYLIST
     if not playl:
         await edit_delete(event, "Playlist empty", time=10)
     else:
-        jep = ""
+        zed = ""
         for num, item in enumerate(playl, 1):
             if item["stream"] == Stream.audio:
-                jep += f"{num}. 🔉  `{item['title']}`\n"
+                zed += f"{num}- 🔉 `{item['title']}`\n"
             else:
-                jep += f"{num}. 📺  `{item['title']}`\n"
-        await edit_delete(event, f"**قائمة التشغيل:**\n\n{jep}\n**الجوكر يتمنى لكم وقتاً ممتعاً**")
+                zed += f"{num}- 📺 `{item['title']}`\n"
+        await edit_delete(event, f"⚈ **قائمـة التشغيـل :**\n\n{zed}")
 
-def convert_youtube_link_to_name(link):
-    with youtube_dl.YoutubeDL({}) as ydl:
-        info = ydl.extract_info(link, download=False)
-        title = info['title']
-    return title
 
-@l313l.ar_cmd(
-    pattern="تشغيل ?(-f)? ?([\S ]*)?",
-    command=("تشغيل", plugin_category),
-    info={
-        "header": "To Play a media as audio on VC.",
-        "description": "To play a audio stream on VC.",
-        "flags": {
-            "-f": "Force play the Audio",
-        },
-        "usage": [
-            "{tr}play (reply to message)",
-            "{tr}play (yt link)",
-            "{tr}play -f (yt link)",
-        ],
-        "examples": [
-            "{tr}play",
-            "{tr}play https://www.youtube.com/watch?v=c05GBLT_Ds0",
-            "{tr}play -f https://www.youtube.com/watch?v=c05GBLT_Ds0",
-        ],
-    },
-)
-async def play_audio(event):
-    "To Play a media as audio on VC."
+@l313l.ar_cmd(pattern="شغل فيديو ?(1)? ?([\S ]*)?")
+async def play_video(event):
     flag = event.pattern_match.group(1)
     input_str = event.pattern_match.group(2)
+
     if input_str == "" and event.reply_to_msg_id:
         input_str = await tg_dl(event)
+        
     if not input_str:
         return await edit_delete(
-            event, "**قم بالرد على ملف صوتي او رابط يوتيوب**", time=20
+            event, "⚈ **قـم بـ إدخـال رابـط مقطع الفيديـو للتشغيـل...**", time=20
         )
+        
     if not vc_player.CHAT_ID:
-        return await edit_or_reply(event, "**`قم بلانضمام للمكالمة اولاً بأستخدام أمر `انضمام")
+        return await edit_or_reply(event, "⚈ **قـم بالانضمـام اولاً الى المكالمـه عبـر الامـر .انضمام**")
+        
+    await edit_or_reply(event, "**╮ جـارِ تشغيـل مقطـٓـع الفيـٓـديو في المكـالمـه... 🎧♥️╰**")
+    
+    if flag == "1":
+        resp = await vc_player.play_song(input_str, Stream.video, force=True)
+    else:
+        resp = await vc_player.play_song(input_str, Stream.video, force=False)
+        
+    if resp:
+        await edit_delete(event, resp, time=30)
+
+
+@l313l.ar_cmd(pattern="شغل ?(1)? ?([\S ]*)?")
+async def play_audio(event):
+    flag = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(2)
+
+    if input_str == "" and event.reply_to_msg_id:
+        input_str = await tg_dl(event)
+        
     if not input_str:
-        return await edit_or_reply(event, "No Input to play in vc")
-    await edit_or_reply(event, "**يتم الان تشغيل الاغنية في الاتصال ❤️**")
-    if flag:
+        return await edit_delete(
+            event, "⚈ **قـم بـ إدخـال رابـط المقطـع الصوتـي للتشغيـل...**", time=20
+        )
+        
+    if not vc_player.CHAT_ID:
+        return await edit_or_reply(event, "⚈ **قـم بالانضمـام الى المكالمـه اولاً**\n⚈ **عبـر الامـر ⤌ ⎞** `.انضمام` **⎝**")
+        
+    await edit_or_reply(event, "**╮ جـارِ تشغيـل المقطـٓـع الصـٓـوتي في المكـالمـه... 🎧♥️╰**")
+    
+    if flag == "1":
         resp = await vc_player.play_song(input_str, Stream.audio, force=True)
     else:
         resp = await vc_player.play_song(input_str, Stream.audio, force=False)
+        
     if resp:
         await edit_delete(event, resp, time=30)
-        
-@l313l.ar_cmd(
-    pattern="ايقاف_مؤقت",
-    command=("ايقاف_مؤقت", plugin_category),
-    info={
-        "header": "To Pause a stream on Voice Chat.",
-        "description": "To Pause a stream on Voice Chat",
-        "usage": [
-            "{tr}pause",
-        ],
-        "examples": [
-            "{tr}pause",
-        ],
-    },
-)
+
+
+@l313l.ar_cmd(pattern="توقف")
 async def pause_stream(event):
-    "To Pause a stream on Voice Chat."
-    await edit_or_reply(event, "**تم ايقاف الموسيقى مؤقتاً ⏸**")
+    await edit_or_reply(event, "⚈ **جـارِ الايقـاف مؤقتـاً ...**")
     res = await vc_player.pause()
     await edit_delete(event, res, time=30)
 
 
-@l313l.ar_cmd(
-    pattern="استمرار",
-    command=("استمرار", plugin_category),
-    info={
-        "header": "To Resume a stream on Voice Chat.",
-        "description": "To Resume a stream on Voice Chat",
-        "usage": [
-            "{tr}resume",
-        ],
-        "examples": [
-            "{tr}resume",
-        ],
-    },
-)
+@l313l.ar_cmd(pattern="كمل")
 async def resume_stream(event):
-    "To Resume a stream on Voice Chat."
-    await edit_or_reply(event, "**تم استمرار الاغنيه استمتع ▶️**")
+    await edit_or_reply(event, "⚈ **جـار الاستئنـاف ...**")
     res = await vc_player.resume()
     await edit_delete(event, res, time=30)
 
 
-@l313l.ar_cmd(
-    pattern="تخطي",
-    command=("تخطي", plugin_category),
-    info={
-        "header": "To Skip currently playing stream on Voice Chat.",
-        "description": "To Skip currently playing stream on Voice Chat.",
-        "usage": [
-            "{tr}skip",
-        ],
-        "examples": [
-            "{tr}skip",
-        ],
-    },
-)
+@l313l.ar_cmd(pattern="تخطي")
 async def skip_stream(event):
-    "To Skip currently playing stream on Voice Chat."
-    await edit_or_reply(event, "**تم تخطي الاغنية وتشغيل الاغنيه التالية 🎵**")
+    await edit_or_reply(event, "⚈ **جـار التخطـي ...**")
     res = await vc_player.skip()
     await edit_delete(event, res, time=30)
-    
 
-@l313l.ar_cmd(
-    pattern="فديو ?(-f)? ?([\S ]*)?",
-    command=("فديو", plugin_category),
-    info={
-        "header": "لتشغيل فيديو في المكالمة الصوتية",
-        "description": "لتشغيل فيديو في المكالمة الصوتية",
-        "flags": {
-            "-f": "التشغيل الإجباري وإيقاف التشغيل الحالي",
-        },
-        "usage": [
-            "{tr}فيد (بالرد على فيديو)",
-            "{tr}فيد (رابط يوتيوب)",
-            "{tr}فيد -f (رابط يوتيوب)",
-        ],
-        "examples": [
-            "{tr}فيد",
-            "{tr}فيد https://www.youtube.com/watch?v=example",
-            "{tr}فيد -f https://www.youtube.com/watch?v=example",
-        ],
-    },
+
+ZelzalMusic_cmd = (
+"**⋆─┄─┄─┄─┄──┄─┄─┄─┄─⋆**\n"
+"⚉ `.شغل`\n"
+"**⪼ الامـر + (كلمـة او رابـط) او بالـرد ع مقطـع صوتـي**\n"
+"⚉ `.شغل فيديو`\n"
+"**⪼ الامـر + (كلمـة او رابـط) او بالـرد ع مقطـع فيديـو**\n\n"
+"**Ⓜ️ اوامـر تشغيـل اجباريـه مـع تخطـي قائمـة التشغيـل :**\n"
+"⚉ `.شغل 1`\n"
+"**⪼ الامـر + (كلمـة او رابـط) او بالـرد ع مقطـع صوتـي**\n"
+"⚉ `.شغل فيديو 1`\n"
+"**⪼ الامـر + (كلمـة او رابـط) او بالـرد ع مقطـع فيديـو**\n\n"
+"⚉ `.قائمة التشغيل`\n"
+"⚉ `.توقف`\n"
+"⚉ `.كمل`\n"
+"⚉ `.تخطي`\n\n"
+"⚉ `.انضمام`\n"
+"⚉ `.خروج`\n\n"
 )
-async def play_video(event):
-    "لتشغيل فيديو في المكالمة الصوتية"
-    flag = event.pattern_match.group(1)
-    input_str = event.pattern_match.group(2)
-    if input_str == "" and event.reply_to_msg_id:
-        input_str = await tg_dl(event)
-    if not input_str:
-        return await edit_delete(
-            event, "**قم بالرد على ملف فيديو او رابط يوتيوب**", time=20
-        )
-    if not vc_player.CHAT_ID:
-        return await edit_or_reply(event, "**`قم بالانضمام للمكالمة أولاً بأستخدام أمر `انضمام**")
-    if not input_str:
-        return await edit_or_reply(event, "لا يوجد مدخل لتشغيله في المكالمة")
-    await edit_or_reply(event, "**يتم الآن تشغيل الفيديو في الاتصال 📺**")
-    if flag:
-        resp = await vc_player.play_song(input_str, Stream.video, force=True)
-    else:
-        resp = await vc_player.play_song(input_str, Stream.video, force=False)
-    if resp:
-        await edit_delete(event, resp, time=30)
-    
+
+@l313l.ar_cmd(pattern="الميوزك")
+async def cmd(zelzallll):
+    await edit_or_reply(zelzallll, ZelzalMusic_cmd)
+
+@l313l.ar_cmd(pattern="ميوزك")
+async def cmd(zelzallll):
+    await edit_or_reply(zelzallll, ZelzalMusic_cmd)
