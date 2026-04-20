4acde19c9ef1980212469aab1c7a1fb1a3ca1528
7rrm <krardh17@gmail.com>
2026-04-20 10:53:40 +0300

    Update vcmanager.py

--- vcmanager.py
+++ vcmanager.py
@@ -17,9 +17,12 @@ async def get_group_call(chat):
 
 
 async def chat_vc_checker(event, chat, edits=True):
-    if isinstance(chat, User):
-        await edit_delete(event, "**- المحـادثـه الصـوتيـه غيـر مدعومـه هنـا ؟!**")
-        return None
+    # ========== تم تعديل هذا الجزء: إزالة منع الخاص ==========
+    # if isinstance(chat, User):
+    #     await edit_delete(event, "**- المحـادثـه الصـوتيـه غيـر مدعومـه هنـا ؟!**")
+    #     return None
+    # ========== نهاية التعديل ==========
+    
     result = await get_group_call(chat)
     if not result:
         if edits:
